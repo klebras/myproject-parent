2d00f9bf3184f3933b2dd806b32d8169dfbf5a2b
kevinLEBRAS <kevin.lb1989@gmail.com>
2017-01-24 14:49:55 +0100

    modif local+github

--- module-web/src/test/java/TestApp.java
+++ module-web/src/test/java/TestApp.java
@@ -4,11 +4,7 @@ public class TestApp {
 	
 	@Test
 	public void testRien(){
-<<<<<<< HEAD
-		System.out.println("modif local");
-=======
-		System.out.println("test github");
->>>>>>> f7f475dc2cced80d777ca9957c28ba8286a3cac3
+		System.out.println("modif local+github");
 	}
 
 }
